7a2d8b7af0aa19b576e43ac18ce9e3a0c31e0675
nadunys <yashnadun@gmail.com>
2020-08-09 14:57:01 +0530

    documentation

---
+++ README.md
@@ -0,0 +1,28 @@
+# Zoom Meeting API
+
+NPM Zoom Meeting API
+
+Functionalities
+
+- Create Meeting
+- Get Meeting List
+- Get a meeting
+- Update meeting
+- Update meeting status
+- Delte meeting
+
+- Get meeting registrants list
+- Add meeting registrant
+- Update meeting registrants' status
+
+## Install
+
+`npm install --save @0xait/zoom-meeting-api`
+
+## Usage
+
+[Visit the documentan](http://the-ai-team.github.io/docs)
+
+## License
+
+Licensed under the MIT Licence.
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -7,24 +7,28 @@ class LoggerClass {
 
   info(message: string, ...data: any) {
     if (this.logLevel >= 4) {
+      // tslint:disable-next-line: no-console
       console.log(`Info: ${message}`, ...data);
     }
   }
 
   debug(message: string, ...data: any) {
     if (this.logLevel >= 3) {
+      // tslint:disable-next-line: no-console
       console.log(`Debug: ${message}`, ...data);
     }
   }
 
   warning(message: string, ...data: any) {
     if (this.logLevel >= 2) {
+      // tslint:disable-next-line: no-console
       console.log(`Warning: ${message}`, ...data);
     }
   }
 
   error(message: string, ...data: any) {
     if (this.logLevel >= 1) {
+      // tslint:disable-next-line: no-console
       console.log(`Error: ${message}`, ...data);
     }
   }
